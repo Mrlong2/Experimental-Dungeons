da352e4fbc68d53032295b772b993faf04249d1e
chas <majorchucklong2@yahoo.com>
2020-04-16 13:02:00 +0900

    It just werks(tm)

--- graphical2/main2.py
+++ graphical2/main2.py
@@ -1,8 +1,12 @@
 import pygame
 from graphical2 import map1gen
 from graphical2 import config
+import os
 import json
 import tcod
+import time
+
+global ACTORS, PROPS, TILES, SELECTED, RUN_GAME, SURFACE_MAIN
 
 
 # keyboard inputs
@@ -15,72 +19,85 @@ def get_inputs():
 
         if event.type == pygame.KEYDOWN:  # if the event is a key down press
             if event.key == pygame.K_UP:
-                SELECTED.dx = (0, 1)
+                SELECTED.dy = -1
 
             if event.key == pygame.K_DOWN:
-                SELECTED.dx = (0, -1)
+                SELECTED.dy = 1
 
             if event.key == pygame.K_LEFT:
-                SELECTED.dx = (1, 0)
+                SELECTED.dx = -1
 
             if event.key == pygame.K_RIGHT:
-                SELECTED.dx = (-1, 0)
+                SELECTED.dx = 1
 
 
 # actors
 # game objects need x, y, type, health, inventory,
 class gameobject:
     def __init__(self, x, y, objtype, sprite=None, spriteoffsetx=0, spriteoffsety=0):
-        #moving and location
+        # moving and location
         self.x = x
         self.y = y
         self.dx = 0
         self.dy = 0
 
-        #object type
+        # object type
         self.type = objtype
         self.sprite = sprite
         self.spriteoffsetx = spriteoffsetx
         self.spriteoffsety = spriteoffsety
 
-
-
-
-    def drawself(self):
-        SURFACE_MAIN.bilt(self.sprite,(self.x * config.CELL_WIDTH, self.y * config.CELL_HEIGHT))
-
-
-
+    def drawself(self, surf):
+        #I LITERALLY SPENT HOURS DEBUGGING THIS WHY CANT I USE SURFACE MAIN, WHY CANT I USE IT?!!?!?!?!?
+        surf.blit(self.sprite, ((self.x * config.CELL_WIDTH)+self.spriteoffsetx, (self.y * config.CELL_HEIGHT)+self.spriteoffsety))
 
 
 # components
-class spritecontroler:
-    pass
+
+#object processing
+def move_objects():
+    global ACTORS
+    for act in ACTORS:
+        if act.dx != 0:
+            act.x += act.dx
+            act.dx = 0
+        if act.dy != 0:
+            act.y += act.dy
+            act.dy = 0
+
+
+
 
 # drawing
 def draw_game():
-    for object in PROPS:
-        object.drawself()
+    # global ACTORS, PROPS, TILES, SELECTED, RUN_GAME, SURFACE_MAIN
+    SURFACE_MAIN.fill((255, 255, 255))
+    for obj in PROPS:
+        obj.drawself(SURFACE_MAIN)
 
-    for object in ACTORS:
-        object.drawself()
-    SURFACE_MAIN.flip()
+    for obj in ACTORS:
+        obj.drawself(SURFACE_MAIN)
+    pygame.display.flip()
 
 
 # game loop
 def game_main_loop():
+    # global ACTORS, PROPS, TILES, SELECTED, RUN_GAME, SURFACE_MAIN
     while RUN_GAME:
+        #start_time = time.time()
         # game states
 
         # check keyboard
         get_inputs()
 
         # process game objects
-
+        move_objects()
         # enemy ai
 
         # draw on screen
         draw_game()
+        #print("FPS: ", 1.0 / (time.time() - start_time))
+
 
 # initialize game
 def game_initialize():
@@ -97,11 +114,12 @@ def game_initialize():
     SELECTED = None
     # Checks whether the game should exit.
     RUN_GAME = True
-    #set the screen
+    # set the screen
     SURFACE_MAIN = pygame.display.set_mode((800, 600))
-    ACTORS, PROPS = map1gen.map_1_generate(10, 10)
+    ACTORS, PROPS, SELECTED = map1gen.map_1_generate(10, 10)
 
 
 if __name__ == '__main__':
+    print("working directory is" + os.getcwd())
     game_initialize()
     game_main_loop()
--- graphical2/map1gen.py
+++ graphical2/map1gen.py
@@ -1,14 +1,18 @@
 import pygame
-from graphical2.main import gameobject
+from graphical2.main2 import gameobject
 from graphical2 import config
 
 
 def map_1_generate(width, height):
     actors = []
     props = []
+    # make a floor
+    for y in range(1, height - 1):
+        for x in range(1, width - 1):
+            floor = [gameobject(x, y, "floor", sprite=config.S_FLOOR)]
+            props += floor
 
-    for y in range(1, height-1):
-        for x in range(1,width-1):
-            props += gameobject(x,y, "floor", sprite=config.S_FLOOR)
+    actors += [gameobject(1, 1, "Adverturer", config.S_GIRL, spriteoffsetx=0, spriteoffsety=-32)]
+    selected = actors[0]
 
-    return actors, props
+    return actors, props, selected
